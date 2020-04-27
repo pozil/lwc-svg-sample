839bb8015756831dd7b5d1c8c2635fd79508f18d
pozil <philippe.ozil@gmail.com>
2020-04-27 13:49:14 +0200

    Initial commit

---
+++ force-app/main/default/lwc/salesforceAwardsInvitation3/salesforceAwardsInvitation3.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import SVG_BASE_URL from '@salesforce/resourceUrl/mySvgs';
+
+export default class SalesforceAwardsInvitation3 extends LightningElement {
+  logoUrl = SVG_BASE_URL + '/logo.svg';
+}
